579da6132049e13cb882c4ab9f71e90e39090db1
brazyko <odintsov.bogdan@gmail.com>
2024-06-15 12:03:18 +0300

    created frontend

--- app/app.py
+++ app/app.py
@@ -1,263 +1,19 @@
-# import streamlit as st
-# import requests
-#
-#
-# # Function to load CSS
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-#
-#
-# # Load CSS file
-# local_css("style.css")
-#
-# # Backend API base URL
-# BASE_URL = "http://localhost:8000/api"  # Adjust this to match your FastAPI server URL
-#
-# AUTH = "auth"
-# USERS = "users"
-#
-#
-# # Register user function
-# def register_user():
-#     st.subheader("Register")
-#
-#     email = st.text_input("Email")
-#     name = st.text_input("Name")
-#     password = st.text_input("Password", type="password")
-#     password_confirm = st.text_input("Confirm Password", type="password")
-#     role = st.text_input("Role")
-#
-#     if st.button("Register"):
-#         if password != password_confirm:
-#             st.error("Passwords do not match")
-#         else:
-#             payload = {
-#                 "email": email,
-#                 "name": name,
-#                 "password": password,
-#                 "passwordConfirm": password_confirm,
-#                 "role": role
-#             }
-#             response = requests.post(f"{BASE_URL}/{AUTH}/register", json=payload)
-#             if response.status_code == 201:
-#                 st.success("User registered successfully!")
-#             else:
-#                 st.error(response.json().get("detail", "Registration failed"))
-#
-#
-# # Login user function
-# def login_user():
-#     st.subheader("Login")
-#
-#     email = st.text_input("Email")
-#     password = st.text_input("Password", type="password")
-#
-#     if st.button("Login"):
-#         payload = {
-#             "email": email,
-#             "password": password
-#         }
-#         response = requests.post(f"{BASE_URL}/{AUTH}/login", json=payload)
-#         if response.status_code == 200:
-#             data = response.json()
-#             st.session_state['access_token'] = data['user_access_token']
-#             st.session_state['refresh_token'] = data['user_refresh_token']
-#             st.success("Logged in successfully!")
-#         else:
-#             st.error(response.json().get("detail", "Login failed"))
-#
-#
-# # Refresh token function
-# def refresh_token():
-#     st.subheader("Refresh Token")
-#
-#     if st.button("Refresh Token"):
-#         headers = {"Authorization": f"Bearer {st.session_state.get('refresh_token')}"}
-#         response = requests.get(f"{BASE_URL}/{AUTH}/refresh", headers=headers)
-#         if response.status_code == 200:
-#             data = response.json()
-#             st.session_state['access_token'] = data['access_token']
-#             st.success("Access token refreshed successfully!")
-#         else:
-#             st.error(response.json().get("detail", "Token refresh failed"))
-#
-#
-# # Logout user function
-# def logout_user():
-#     st.subheader("Logout")
-#
-#     if st.button("Logout"):
-#         headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#         response = requests.get(f"{BASE_URL}/{AUTH}/logout", headers=headers)
-#         if response.status_code == 200:
-#             st.session_state.clear()
-#             st.success("Logged out successfully!")
-#         else:
-#             st.error(response.json().get("detail", "Logout failed"))
-#
-#
-# # Get current user info function
-# def get_me():
-#     st.subheader("Get Current User Info")
-#
-#     headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#     response = requests.get(f"{BASE_URL}/{USERS}/me", headers=headers)
-#     if response.status_code == 200:
-#         user_info = response.json().get("user", {})
-#         if user_info:
-#             st.markdown("""
-#                         <style>
-#                         .user-info {
-#                             font-size: 24px;
-#                             color: white;
-#                             margin-bottom: 10px;
-#                         }
-#                         .user-info-label {
-#                             font-weight: bold;
-#                             color: #a0a0a0;
-#                         }
-#                         </style>
-#                     """, unsafe_allow_html=True)
-#
-#             st.markdown(f"""
-#                         <div class="user-info"><span class="user-info-label">Name:</span> {user_info.get('name')}</div>
-#                         <div class="user-info"><span class="user-info-label">Email:</span> {user_info.get('email')}</div>
-#                         <div class="user-info"><span class="user-info-label">Role:</span> {user_info.get('role')}</div>
-#                         <div class="user-info"><span class="user-info-label">Verified:</span> {user_info.get('verified')}</div>
-#                         <div class="user-info"><span class="user-info-label">Created At:</span> {user_info.get('created_at')}</div>
-#                         <div class="user-info"><span class="user-info-label">Updated At:</span> {user_info.get('updated_at')}</div>
-#                     """, unsafe_allow_html=True)
-#         else:
-#             st.error("No user info available.")
-#     else:
-#         st.error(response.json().get("detail", "Failed to get user info"))
-#
-#
-# def get_all_users():
-#     st.subheader("Get Current User Info")
-#
-#     headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#     response = requests.get(f"{BASE_URL}/{USERS}/me", headers=headers)
-#     if response.status_code == 200:
-#         user_info = response.json().get("user", {})
-#         if user_info:
-#             st.markdown("""
-#                         <style>
-#                         .user-info {
-#                             font-size: 24px;
-#                             color: white;
-#                             margin-bottom: 10px;
-#                         }
-#                         .user-info-label {
-#                             font-weight: bold;
-#                             color: #a0a0a0;
-#                         }
-#                         </style>
-#                     """, unsafe_allow_html=True)
-#
-#             st.markdown(f"""
-#                         <div class="user-info"><span class="user-info-label">Name:</span> {user_info.get('name')}</div>
-#                         <div class="user-info"><span class="user-info-label">Email:</span> {user_info.get('email')}</div>
-#                         <div class="user-info"><span class="user-info-label">Role:</span> {user_info.get('role')}</div>
-#                         <div class="user-info"><span class="user-info-label">Verified:</span> {user_info.get('verified')}</div>
-#                         <div class="user-info"><span class="user-info-label">Created At:</span> {user_info.get('created_at')}</div>
-#                         <div class="user-info"><span class="user-info-label">Updated At:</span> {user_info.get('updated_at')}</div>
-#                     """, unsafe_allow_html=True)
-#         else:
-#             st.error("No user info available.")
-#     else:
-#         st.error(response.json().get("detail", "Failed to get user info"))
-#
-#
-# # Delete all users function
-# def delete_all_users():
-#     st.subheader("Delete All Users")
-#
-#     if st.button("Delete All Users"):
-#         headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#         response = requests.delete(f"{BASE_URL}/{USERS}/delete-users", headers=headers)
-#         if response.status_code == 200:
-#             st.success("All users deleted successfully!")
-#         else:
-#             st.error(response.json().get("detail", "Failed to delete all users"))
-#
-#
-# # Get all users function
-# def get_all_users():
-#     st.subheader("Get All Users")
-#
-#     headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#     response = requests.get(f"{BASE_URL}/{USERS}", headers=headers)
-#     if response.status_code == 200:
-#         users = response.json()
-#         for user in users:
-#             st.write(user)
-#     else:
-#         st.error(response.json().get("detail", "Failed to get all users"))
-#
-#
-# # Get user details function
-# def get_user_details():
-#     st.subheader("Get User Details")
-#
-#     user_id = st.text_input("User ID")
-#     if st.button("Get User Details"):
-#         headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
-#         response = requests.get(f"{BASE_URL}/{USERS}/{user_id}", headers=headers)
-#         if response.status_code == 200:
-#             user_details = response.json()
-#             st.write(user_details)
-#         else:
-#             st.error(response.json().get("detail", "Failed to get user details"))
-#
-# # Main app function
-# def main():
-#     st.title("FastAPI with JWT Authentication")
-#
-#     menu = ["Register", "Login", "Refresh Token", "Logout", "Get User Info", "Delete All Users", "Get All Users", "Get User Details"]
-#     choice = st.sidebar.selectbox("Menu", menu)
-#
-#     if choice == "Register":
-#         register_user()
-#     elif choice == "Login":
-#         login_user()
-#     elif choice == "Refresh Token":
-#         refresh_token()
-#     elif choice == "Logout":
-#         logout_user()
-#     elif choice == "Get User Info":
-#         get_me()
-#     elif choice == "Get All Users":
-#         get_all_users()
-#     elif choice == "Get User Details":
-#         get_user_details()
-#     elif choice == "Delete All Users":
-#         delete_all_users()
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
 import streamlit as st
 import requests
 
-# Function to load CSS
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# Load CSS file
+
 local_css("style.css")
 
-# Backend API base URL
-BASE_URL = "http://localhost:8000/api"  # Adjust this to match your FastAPI server URL
-
+BASE_URL = "http://localhost:8000/api"
 AUTH = "auth"
 USERS = "users"
 
-# Register user function
+
 def register_user():
     st.subheader("Register")
 
@@ -284,7 +40,7 @@ def register_user():
             else:
                 st.error(response.json().get("detail", "Registration failed"))
 
-# Login user function
+
 def login_user():
     st.subheader("Login")
 
@@ -305,7 +61,7 @@ def login_user():
         else:
             st.error(response.json().get("detail", "Login failed"))
 
-# Refresh token function
+
 def refresh_token():
     st.subheader("Refresh Token")
 
@@ -319,7 +75,7 @@ def refresh_token():
         else:
             st.error(response.json().get("detail", "Token refresh failed"))
 
-# Logout user function
+
 def logout_user():
     st.subheader("Logout")
 
@@ -332,9 +88,9 @@ def logout_user():
         else:
             st.error(response.json().get("detail", "Logout failed"))
 
-# Get current user info function
+
 def get_me():
-    st.subheader("Get Current User Info")
+    st.subheader("Get My profile")
 
     headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
     response = requests.get(f"{BASE_URL}/{USERS}/me", headers=headers)
@@ -368,7 +124,7 @@ def get_me():
     else:
         st.error(response.json().get("detail", "Failed to get user info"))
 
-# Delete all users function
+
 def delete_all_users():
     st.subheader("Delete All Users")
 
@@ -380,9 +136,9 @@ def delete_all_users():
         else:
             st.error(response.json().get("detail", "Failed to delete all users"))
 
-# Get all users function
+
 def get_all_users():
-    st.subheader("Get All Users (ID, Email, Name)")
+    st.subheader("List of users")
 
     headers = {"Authorization": f"Bearer {st.session_state.get('access_token')}"}
     response = requests.get(f"{BASE_URL}/{USERS}/users-list/", headers=headers)
@@ -394,7 +150,8 @@ def get_all_users():
 
             # Select desired columns efficiently using list comprehension
             selected_users = [
-                {"ID": user["id"], "Email": user["email"], "Name": user.get("name", ""), "Created at": user["created_at"]}
+                {"ID": user["id"], "Email": user["email"], "Name": user.get("name", ""),
+                 "Created at": user["created_at"]}
                 for user in users
             ]
             # Check if selected_users has data
@@ -411,8 +168,6 @@ def get_all_users():
         st.error(response.json().get("detail", "Failed to get all users"))
 
 
-
-# Get user details function
 def get_user_details():
     st.subheader("Get User Details")
 
@@ -451,11 +206,11 @@ def get_user_details():
             st.error(response.json().get("detail", "Failed to get user info"))
 
 
-# Main app function
 def main():
     st.title("FastAPI with JWT Authentication")
 
-    menu = ["Register", "Login", "Refresh Token", "Logout", "Get User Info", "Delete All Users", "Get All Users", "Get User Details"]
+    menu = ["Register", "Login", "Refresh Token", "Logout", "My profile", "Delete All Users", "Get All Users",
+            "Get User Details"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Register":
@@ -466,7 +221,7 @@ def main():
         refresh_token()
     elif choice == "Logout":
         logout_user()
-    elif choice == "Get User Info":
+    elif choice == "My profile":
         get_me()
     elif choice == "Delete All Users":
         delete_all_users()
@@ -478,4 +233,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- readMe.md
+++ readMe.md
@@ -5,8 +5,12 @@
 
 ### install docker desktop or docker
 
-### run docker run -d --name fastapi -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password123 -p 27017:27017 mongo in terminal
+### в консолі команда docker run -d --name fastapi -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password123 -p 27017:27017 mongo in terminal
 
-### run pip install -r requirements.txt in terminal
+### в консолі . .\venv\Scripts\activate
 
-### run uvicorn app.main:app --reload in terminal
+### в консолі команда -  uvicorn app.main:app --reload 
+
+
+### в консолі іншій cd app
+### і потім в тій же новій консолі команда streamlit run app.py
